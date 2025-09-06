8ff8dd034c99cdd928cd840cb7dce89308ecc781
plumbnebula <114823220+plumbnebula5741@users.noreply.github.com>
2025-09-06 14:53:47 +0200

    Add files via upload

---
+++ src/main/java/plumb/minecartdupe/MinecartDupe.java
@@ -0,0 +1,17 @@
+package plumb.minecartdupe;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinecartDupe extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
